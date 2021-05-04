059932ff52fa704df9db1e5ea67cfd467f6d8ac5
engmoamenragab <eng.moamen.ragab@gmail.com>
2021-05-04 02:48:26 +0200

    Finalize the project

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 //ANCHOR declare variables
 let links = document.querySelectorAll("nav .nav-link"),
   dataRow = document.getElementById("dataRow"),
-  recipesDataContent = [];
-
+  singleRecipeRow = document.getElementById("singleRecipeRow"),
+  recipesDataContent = [],
+  singleRecipeDataContent = [];
 //ANCHOR get recipes data
-async function getRecipes(theWord) {
+async function getRecipes(theWord = "melon") {
   let recipes = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${theWord}`),
     recipesData = await recipes.json();
   recipesDataContent = recipesData.recipes;
   displayRecipes();
 }
-getRecipes("onion");
+getRecipes();
 //ANCHOR change recipes function
 for (let i = 0; i < links.length; i++) {
   links[i].addEventListener("click", function (eventInfo) {
@@ -22,14 +23,13 @@ for (let i = 0; i < links.length; i++) {
 function displayRecipes() {
   let content = "";
   for (let i = 0; i < recipesDataContent.length; i++) {
-    content += `        <div class="col-md-4">
-    <div class="recipe p-3">
+    content += `        <div class="col-md-6 col-lg-4">
+    <div class="recipeItem p-3">
       <div class="card">
         <div class="card-header">${recipesDataContent[i].recipe_id}</div>
-        <img src="${recipesDataContent[i].image_url}" class="card-img-top" alt="recipe" />
-        <div class="card-body text-center">
+        <img src="${recipesDataContent[i].image_url}" onclick="getSingleRecipe('${recipesDataContent[i].recipe_id}')" class="card-img-top" alt="recipe" data-toggle="modal" data-target="#exampleModal" />
+        <div class="card-body">
           <h5 class="card-title">${recipesDataContent[i].title}</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
         </div>
         <div class="card-footer text-muted">
           <footer class="blockquote-footer">
@@ -41,4 +41,31 @@ function displayRecipes() {
   </div>`;
     dataRow.innerHTML = content;
   }
+}
+//ANCHOR get single recipe data
+async function getSingleRecipe(recipeId) {
+  let singleRecipe = await fetch(`https://forkify-api.herokuapp.com/api/get?rId=${recipeId}`),
+    singleRecipeData = await singleRecipe.json();
+  singleRecipeDataContent = singleRecipeData.recipe;
+  displaySingleRecipe();
+}
+//ANCHOR display single recipe function
+function displaySingleRecipe() {
+  let content = "";
+  content += `            <div class="singleRecipe p-3">
+  <div class="card">
+    <div class="card-header">${singleRecipeDataContent.recipe_id}</div>
+    <img src="${singleRecipeDataContent.image_url}" class="card-img-top" alt="recipe" />
+    <div class="card-body">
+      <h5 class="card-title">${singleRecipeDataContent.title}</h5>
+      <p class="card-text">${singleRecipeDataContent.ingredients}</p>
+    </div>
+    <div class="card-footer text-muted">
+      <footer class="blockquote-footer">
+        ${singleRecipeDataContent.publisher} <cite title="Source Title"><a href="${singleRecipeDataContent.publisher_url}" class="btn btn-primary">Source Title</a></cite>
+      </footer>
+    </div>
+  </div>
+</div>`;
+  singleRecipeRow.innerHTML = content;
 }
